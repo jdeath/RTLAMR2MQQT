a4c58a5a86e55ed85ca91279c02e23c95dbcbfb3
jdeath <17914369+jdeath@users.noreply.github.com>
2020-01-25 08:13:27 -0500

    Add files via upload
    Update to only update if value changed. Not a perfect solution if have more than one sensor, but I have one that repeats every second and another which only updates a couple times a month (battery powered gas meter)

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -24,16 +24,20 @@ echo "MQTT Device IDs =" $MQTT_IDS
 /usr/local/bin/rtl_tcp &>/dev/null &
 
 sleep 10
+LASTVAL="0"
 
 /go/bin/rtlamr -format json -msgtype=$MQTT_MSGTYPE -filterid=$MQTT_IDS | while read line
+
 do
   VAL="$(echo $line | jq --raw-output '.Message.Consumption' | tr -s ' ' '_')" # replace ' ' with '_'
   DEVICEID="$(echo $line | jq --raw-output '.Message.ID' | tr -s ' ' '_')"
-  
-  
   MQTT_PATH="readings/$DEVICEID/meter_reading"
 
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
-  echo $VAL | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_PATH
+  if [ "$VAL" != "$LASTVAL" ]; then
+    echo $VAL | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_PATH
+	LASTVAL=$VAL
+  fi
+  
 done
