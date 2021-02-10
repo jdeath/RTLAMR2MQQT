336a86eb8ef83156e7b5d7d133eaa753bdba78ae
Anthony Pluth <2065118-abpluth@users.noreply.gitlab.com>
2021-02-10 14:38:00 -0600

    add parameter for MQTT port

--- RTLAMR2MQQT/rtl2mqtt.sh
+++ RTLAMR2MQQT/rtl2mqtt.sh
@@ -5,6 +5,7 @@ PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 CONFIG_PATH=/data/options.json
 MQTT_HOST="$(jq --raw-output '.mqtt_host' $CONFIG_PATH)"
+MQTT_PORT="$(jq --raw-output '.mqtt_port' $CONFIG_PATH)"
 MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)"
 MQTT_PASS="$(jq --raw-output '.mqtt_password' $CONFIG_PATH)"
 MQTT_MSGTYPE="$(jq --raw-output '.msgType' $CONFIG_PATH)"
@@ -14,6 +15,7 @@ MQTT_IDS="$(jq --raw-output '.ids' $CONFIG_PATH)"
 # Start the listener and enter an endless loop
 echo "Starting RTLAMR with parameters:"
 echo "MQTT Host =" $MQTT_HOST
+echo "MQTT Port =" $MQTT_PORT
 echo "MQTT User =" $MQTT_USER
 echo "MQTT Password =" $MQTT_PASS
 echo "MQTT Message Type =" $MQTT_MSGTYPE
@@ -31,7 +33,7 @@ LASTVAL="0"
 # set a time to listen for. Set to 0 for unliminted
 
 # Do this loop, so will restart if buffer runs out
-while true; do 
+while true; do
 
 /go/bin/rtlamr -format json -msgtype=$MQTT_MSGTYPE -filterid=$MQTT_IDS | while read line
 
@@ -43,10 +45,10 @@ do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
   if [ "$VAL" != "$LASTVAL" ]; then
-    echo $VAL | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_PATH
+    echo $VAL | /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_PATH
 	LASTVAL=$VAL
   fi
-  
+
 done
 
 sleep 60
